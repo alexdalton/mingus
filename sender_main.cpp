9b8ba13038a54c9c1bb600cc7fcf64bc8d5f06ad
Pascal Lis <lis2@siebl-0220-14.ews.illinois.edu>
2014-05-05 20:58:47 -0500

    work done on sender

--- sender_main.cpp
+++ sender_main.cpp
@@ -96,7 +96,7 @@ void *receive_function(void * inport)
 }
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename,
-					  unsigned long long int bytesToTransfer)
+                      unsigned long long int bytesToTransfer)
 {
     pthread_t send_thread, receive_thread;
     int iret1, iret2;
@@ -120,18 +120,18 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
 
 int main(int argc, char** argv)
 {
-	unsigned short int udpPort;
-	unsigned long long int numBytes;
-	
-	if(argc != 5)
-	{
-		fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
-		exit(1);
-	}
-	
-	udpPort = (unsigned short int)atoi(argv[2]);
-	numBytes = atoll(argv[4]);
-	
-	reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
+    unsigned short int udpPort;
+    unsigned long long int numBytes;
+    
+    if(argc != 5)
+    {
+        fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
+        exit(1);
+    }
+    
+    udpPort = (unsigned short int)atoi(argv[2]);
+    numBytes = atoll(argv[4]);
+    
+    reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
     return 0;
 }
