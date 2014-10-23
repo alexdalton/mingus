cdd71f830db01365ec7a1ab97abe2aa0b67d6f4f
Alex Dalton <alexmdalton@gmail.com>
2014-10-22 21:46:07 -0500

    kernel for 5p1 done

--- fa2014/lab5-reduction-and-prefix-scan/5.1-reduction/kernel.cu
+++ fa2014/lab5-reduction-and-prefix-scan/5.1-reduction/kernel.cu
@@ -15,19 +15,40 @@ __global__ void reduction(float *out, float *in, unsigned size)
     Traverse the reduction tree
     Write the computed sum to the output vector at the correct index
     ********************************************************************/
-    __shared__ float scan_array[BLOCK_SIZE];
-    scan_array[threadIdx.x] = in[threadIdx.x];
+    __shared__ float partialSum[2 * BLOCK_SIZE];
+    unsigned int tx = threadIdx.x;
+    unsigned int start = 2 * blockIdx.x * blockDim.x;
+    if ((start + tx) < size)
+    {
+        partialSum[tx] = in[start + tx];
+    }  
+    else
+    {
+        partialSum[tx] = 0.0f;
+    }
+    if ((start + blockDim.x + tx) < size)
+    {
+        partialSum[blockDim.x + tx] = in[start + blockDim.x + tx];
+    }
+    else
+    {
+        partialSum[blockDim.x + tx] = 0.0f;
+    }
     __syncthreads();
 
     int stride = 1;
-    while (stride < BLOCK_SIZE)
+    while (stride < 2 * BLOCK_SIZE)
     {
-    	int index = (threadIdx.x + 1) * stride * 2 - 1;
-    	if (index < BLOCK_SIZE)
+    	int index = (tx + 1) * stride * 2 - 1;
+    	if (index < 2* BLOCK_SIZE)
     	{
-    		scan_array[index] += scan_array[index - stride];
-    		stride *= 2;
+    		partialSum[index] += partialSum[index - stride];
     	}
+    	stride *= 2;
     	__syncthreads();
     }
+
+    if (tx == 0)
+        out[blockIdx.x] = partialSum[2 * BLOCK_SIZE - 1];
+
 }
