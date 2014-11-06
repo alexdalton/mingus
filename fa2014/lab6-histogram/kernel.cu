aa4913d7c5bf6e22146397629fd59cbb4c6b34aa
Alex Dalton <alexmdalton@gmail.com>
2014-11-05 23:08:51 -0600

    lab6 day 1

--- fa2014/lab6-histogram/kernel.cu
+++ fa2014/lab6-histogram/kernel.cu
@@ -5,61 +5,59 @@
  *cr                         All Rights Reserved
  *cr
  ******************************************************************************/
+#define BLOCK_SIZE 512
 
-// Define your kernels in this file you may use more than one kernel if you
-// need to
+__device__ void atomicIncrementInt8(uint8_t* address)
+{
+    unsigned int *base_address = (unsigned int *)((size_t)address & ~3);
+    unsigned int selectors[] = {0x3214, 0x3240, 0x3410, 0x4210};
+    unsigned int sel = selectors[(size_t)address & 3];
+    unsigned int old, assumed, add_, new_;
+    uint8_t val = 0x01;
+    old = *base_address;
+    do {
+        assumed = old;
+        add_ = (uint8_t)__byte_perm(old, 0, ((size_t)address & 3)) + val;
+        if (add_ > 255)
+            add_ = 255;
+        new_ = __byte_perm(old, add_, sel);
+        old = atomicCAS(base_address, assumed, new_);
+    } while (assumed != old);
+}
 
-// INSERT KERNEL(S) HERE
+__global__ void histo_kernel(unsigned int* input, uint8_t* bins, unsigned int num_elements,
+                             unsigned int num_bins)
+{
+    /* Version 1
+    unsigned int start = (blockIdx.x * BLOCK_SIZE + threadIdx.x) * 6;
+    for (int i = start; i < start + 6; i++)
+    {
+        if (i < num_elements)
+            atomicIncrementInt8(&bins[input[i]]);
+    }*/
 
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = blockDim.x * gridDim.x;
+    while (i < num_elements)
+    {
+        atomicIncrementInt8(&bins[input[i]]);
+        i += stride;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 /******************************************************************************
 Setup and invoke your kernel(s) in this function. You may also allocate more
 GPU memory if you need to
 *******************************************************************************/
 void histogram(unsigned int* input, uint8_t* bins, unsigned int num_elements,
-        unsigned int num_bins) {
+        unsigned int num_bins) 
+{
+    /* Version 1
+    dim3 dimBlock(BLOCK_SIZE, 1, 1);
+    dim3 dimGrid((num_elements - 1) / (6 * BLOCK_SIZE) + 1, 1, 1);*/
 
-    // INSERT CODE HERE
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    dim3 dimBlock(BLOCK_SIZE, 1, 1);
+    dim3 dimGrid((num_elements + 1) / (BLOCK_SIZE * 2) + 1, 1, 1);
+    histo_kernel<<<dimGrid, dimBlock>>>(input, bins, num_elements, num_bins);    
 }
-
-
